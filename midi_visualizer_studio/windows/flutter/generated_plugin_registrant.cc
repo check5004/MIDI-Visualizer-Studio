272bd4af824cd8e4222409315d2464552571b1ac
check5004 <check50045366@gmail.com>
2025-12-02 23:37:22 +0900

    feat: インスペクターパネルとMIDIバインディング機能を追加し、関連するFlutterプラグインを導入

--- midi_visualizer_studio/windows/flutter/generated_plugin_registrant.cc
+++ midi_visualizer_studio/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,15 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <device_manager/device_manager_plugin.h>
 #include <flutter_acrylic/flutter_acrylic_plugin.h>
 #include <screen_retriever_windows/screen_retriever_windows_plugin_c_api.h>
 #include <universal_ble/universal_ble_plugin_c_api.h>
 #include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  DeviceManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DeviceManagerPlugin"));
   FlutterAcrylicPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterAcrylicPlugin"));
   ScreenRetrieverWindowsPluginCApiRegisterWithRegistrar(
--- midi_visualizer_studio/windows/flutter/generated_plugins.cmake
+++ midi_visualizer_studio/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  device_manager
   flutter_acrylic
   screen_retriever_windows
   universal_ble
